6e04504dd5d13be0bdcee4f83f7dcb7e121923ac
sasuke <tryvasan02@gmail.com>
2024-07-23 16:06:19 +0530

    demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <div id="site-content">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/review" element={<ReviewPage/>} />
